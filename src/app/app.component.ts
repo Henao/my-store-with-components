eb12a0abe3e27c7596219e707087f0d55d5dbf16
Julian <Henao>
2021-12-08 15:17:48 -0500

    Crea nuevo componente para visualización del producto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
-
+import { Product } from './models/product.model'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  imgParent = '';
+  products: Product[]=[
+    {
+      id: '1',
+      name: 'Cámara',
+      image: './assets/images/toy.jpg',
+      price: 200
+    },
+    {
+      id: '2',
+      name: 'Patines',
+      image: './assets/images/toy.jpg',
+      price: 500
+    },
+    {
+      id: '3',
+      name: 'Pantalla',
+      image: './assets/images/toy.jpg',
+      price: 120
+    },
+    {
+      id: '4',
+      name: 'Libros Allan Poe',
+      image: './assets/images/toy.jpg',
+      price: 550
+    }
+  
+  ]
+
+
+  onLoaded(img: string){
+    console.log('Log padre', img);
+  }
+
+
 }
